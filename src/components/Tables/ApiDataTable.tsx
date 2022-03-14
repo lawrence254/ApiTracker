8002af36fe818af01c46a7a1bbb45d37298c6105
lawrence254 <lawrenslava1@gmail.com>
2022-03-14 16:45:39 +0300

    Partial: Resolve 7a9db86 partially. Page has to load then select the number of rows to show so that you can view the data table. Should be fixed

--- src/components/Tables/ApiDataTable.tsx
+++ src/components/Tables/ApiDataTable.tsx
@@ -1,60 +1,54 @@
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from '@mui/material';
 import { ChangeEvent, useEffect, useState } from 'react';
+import { DataResponse } from '../../interfaces/ApiModel';
 import { NavBar } from '../../services/config/navigation';
+import Row from './Row';
 const axios = require('axios');
 
 export default function ApiDataTable() {
-    // const [response, setResponse] = useState<DataResponse[]>([])
-    const [response,setResponse]=useState([])
+    const [sliced, setSliced] = useState([])
+    const [data, setData] = useState([]);
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(5);
     const [startRow, setStartRow] = useState(0);
     const [maxPageRow, setMaxPageRow] = useState(rowsPerPage);
-    
-    
-        let rows:any = [];
-        let slicedRows:any=[];
+
+
+    let rows: any = [];
+    let slicedRows: any = [];
     useEffect(() => {
 
-        const fetchData=async()=>{
-            let data=await  axios.get('http://localhost:8080/results')
-            setResponse(data.data)
-            rows=data.data;
+        const fetchData = async () => {
+            const { data: response } = await axios.get('http://localhost:8080/results')
+            console.log(response)
+            setData(response)
         }
 
         fetchData()
     }, [])
-console.log(rows)
-    response.forEach((obj)=>{
-        // rows.push(obj)
-    })
 
     const sliceRows = (maxRows: number) => {
-        let intermidiary = rows.slice(startRow, maxRows);
-        slicedRows.push(intermidiary);
+        let intermidiary = data.slice(startRow, maxRows);
+        setSliced(intermidiary)
         console.log(intermidiary)
     }
-    console.log(rows)
-    console.log(slicedRows)
-    
-    
-    const handleChangePage = (event: unknown,newPage:number) => {
+
+
+    const handleChangePage = (event: unknown, newPage: number) => {
         setPage(newPage);
         setStartRow(maxPageRow + 1);
         slicedRows.push(rows.slice(startRow, maxPageRow))
         // console.log(setPage(newPage))
     };
-    // console.log(slicedRows)
-// {slicedRows.map((row:any) => (
-//                                 <Row key={row.keyID} row={row} />
-//                             ))}
-    const handleChangeRowsPerPage = (event:ChangeEvent<HTMLInputElement>) => {
+    console.log(sliced)
+
+    const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
         setRowsPerPage(parseInt(event.target.value, 10));
         setPage(0);
         sliceRows(parseInt(event.target.value, 10));
         setMaxPageRow(parseInt(event.target.value, 10));
     };
-    
+
     return (
         <div className="tableContainer">
             <NavBar />
@@ -70,14 +64,17 @@ console.log(rows)
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            
+                            {sliced.map((row: any) => (
+                                <Row key={row.keyID} row={row} />
+                            ))}
+
                         </TableBody>
                     </Table>
                 </TableContainer>
                 <TablePagination
                     rowsPerPageOptions={[5, 10, 25]}
                     component="div"
-                    count={rows.length}
+                    count={data.length}
                     rowsPerPage={rowsPerPage}
                     page={page}
                     onPageChange={handleChangePage}
